68cb6cd31c7012d3cd19b36818ec952918a118b0
raj-gami619 <32761987+raj-gami619@users.noreply.github.com>
2020-07-07 12:01:43 +0300

    Initial Commit
    Complete Project

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int a, b, c, t;
+
+{
+	printf("\nEnter the Multiplicand :");
+    scanf("%d", &a);
+
+	printf("\nEnter Multiplier :");
+    scanf("%d", &t);
+    printf("\n");
+
+	b=0;
+}
+	for(c=a; c<=(a*t); c+=a)
+{
+    b++;
+	printf("%d x %d = %d\n", a, b, c);
+}
+    getch();
+    return 0;
+}
